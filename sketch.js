b4acab6466b1fccdb96c98ac77a13d64783052b2
Oriol Pérez <oriol@fxstreet.com>
2024-03-07 12:06:58 +0100

    up

--- sketch.js
+++ sketch.js
@@ -7,38 +7,16 @@ let boardSquares = [];
 let lightBouncingBall = null;
 let darkBouncingBall = null;
 let ballDiameter = 0;
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  xUnit = windowWidth / BOARD_SIZE;
-  yUnit = windowHeight / BOARD_SIZE;
-  lightColor = color(200);
-  darkColor = color(50);
-  ballDiameter = min(xUnit, yUnit);
-  initBoardSquares();
-  lightBouncingBall = new BouncingBall(lightColor);
-  darkBouncingBall = new BouncingBall(darkColor);
-};
-function draw() {
-  renderBoardSquares();
-  darkBouncingBall.move();  
-  darkBouncingBall.render();
-  lightBouncingBall.move();
-  lightBouncingBall.render();
-};
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  xUnit = windowWidth / BOARD_SIZE;
-  yUnit = windowHeight / BOARD_SIZE;
-  ballDiameter = min(xUnit, yUnit);
-};
+
 function initBoardSquares() {
   for (let i = 0; i < BOARD_SIZE; i++) {
     boardSquares.push([]);
     for (let j = 0; j < BOARD_SIZE; j++) {
       boardSquares[i].push({ color: Math.random() > 0.5 ? lightColor : darkColor, });
-    }
-  }
+    };
+  };
 };
+
 class BouncingBall {
   constructor(colorP, xVelocityP, yVelocityP) {
     this.x = floor(random(BOARD_SIZE));
@@ -50,17 +28,31 @@ class BouncingBall {
     this.xVelocity = xVelocityP;
     this.yVelocity = yVelocityP;
     this.color = colorP;
-    this.move = () => {
-
-    };
-    this.render = () => {
-      noStroke();
-      fill(this.color);
-      ellipse((this.x + 0.5) * xUnit, (this.y + 0.5) * yUnit, ballDiameter, ballDiameter);
-    };
+  };
+  move = () => {
+    const nextX = this.x + this.xVelocity;
+    const nextY = this.y + this.yVelocity;
+    if (nextX < 0 || nextX >= BOARD_SIZE) {
+      this.xVelocity *= -1;
+    } else if (boardSquares[nextX][this.y].color === this.color) {
+      this.xVelocity *= -1;
+      boardSquares[nextX][this.y].color = this.color === lightColor ? darkColor : lightColor;
+    }
+    if (nextY < 0 || nextY >= BOARD_SIZE) {
+      this.yVelocity *= -1;
+    } else if (boardSquares[this.x][nextY].color === this.color) {
+      this.yVelocity *= -1;
+      boardSquares[this.x][nextY].color = this.color === lightColor ? darkColor : lightColor;
+    }
+    this.x += this.xVelocity;
+    this.y += this.yVelocity;
+  };
+  render = () => {
+    noStroke();
+    fill(this.color);
+    ellipse((this.x + 0.5) * xUnit, (this.y + 0.5) * yUnit, ballDiameter, ballDiameter);
   };
 };
-
 
 function renderBoardSquares() {
   for (let i = 0; i < BOARD_SIZE; i++) {
@@ -70,4 +62,31 @@ function renderBoardSquares() {
       rect(i * xUnit, j * yUnit, xUnit, yUnit);
     };
   };
+};
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  xUnit = windowWidth / BOARD_SIZE;
+  yUnit = windowHeight / BOARD_SIZE;
+  lightColor = color(200);
+  darkColor = color(50);
+  ballDiameter = min(xUnit, yUnit);
+  initBoardSquares();
+  lightBouncingBall = new BouncingBall(lightColor,1,1);
+  darkBouncingBall = new BouncingBall(darkColor,-1,-1);
+};
+
+function draw() {
+  renderBoardSquares();
+  darkBouncingBall.move();
+  darkBouncingBall.render();
+  lightBouncingBall.move();
+  lightBouncingBall.render();
+};
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  xUnit = windowWidth / BOARD_SIZE;
+  yUnit = windowHeight / BOARD_SIZE;
+  ballDiameter = min(xUnit, yUnit);
 };
