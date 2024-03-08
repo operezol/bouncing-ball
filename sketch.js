5d9653746ccdc389e46360bd612a21d22158b661
operezol <oriolperezolivares@gmail.com>
2024-03-08 10:38:50 +0100

    up

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const BOARD_SIZE = 100;
+const BOARD_SIZE = 200;
 let xUnit = 0;
 let yUnit = 0;
 let lightColor;
@@ -7,16 +7,16 @@ let boardSquares = [];
 let lightBouncingBall = null;
 let darkBouncingBall = null;
 let ballDiameter = 0;
-
 function initBoardSquares() {
   for (let i = 0; i < BOARD_SIZE; i++) {
     boardSquares.push([]);
     for (let j = 0; j < BOARD_SIZE; j++) {
-      boardSquares[i].push({ color: Math.random() > 0.5 ? lightColor : darkColor, });
-    };
-  };
-};
-
+      boardSquares[i].push({
+        color: Math.random() > 0.5 ? lightColor : darkColor,
+      });
+    }
+  }
+}
 class BouncingBall {
   constructor(colorP, xVelocityP, yVelocityP) {
     this.x = floor(random(BOARD_SIZE));
@@ -24,49 +24,78 @@ class BouncingBall {
     while (boardSquares[this.x][this.y].color === colorP) {
       this.x = floor(random(BOARD_SIZE));
       this.y = floor(random(BOARD_SIZE));
-    };
+    }
     this.xVelocity = xVelocityP;
     this.yVelocity = yVelocityP;
     this.color = colorP;
+    this.stationary={
+      x:true,
+      y:true
+    };
+  }
+
+  bounceHorizontally = () => {
+    this.xVelocity *= -1;
   };
+
+  bounceVertically = () => {
+    this.yVelocity *= -1;
+  };
+
   move = () => {
     const nextX = this.x + this.xVelocity;
     const nextY = this.y + this.yVelocity;
-    if (nextX < 0 || nextX >= BOARD_SIZE) {
-      this.xVelocity *= -1;
-    } else if (boardSquares[nextX][this.y].color === this.color) {
-      this.xVelocity *= -1;
-      boardSquares[nextX][this.y].color = this.color === lightColor ? darkColor : lightColor;
+    const isNextXInBounds = nextX >= 0 && nextX < BOARD_SIZE;
+    const isNextYInBounds = nextY >= 0 && nextY < BOARD_SIZE;
+    const oppositeColor = this.color === lightColor ? darkColor : lightColor;
+  
+    this.stationary.x = true; 
+
+    if (!isNextXInBounds) {
+      this.bounceHorizontally();
+    } else if (boardSquares[nextX][this.y].color === oppositeColor) {
+      this.bounceHorizontally();
+      boardSquares[nextX][this.y].color = this.color;
+    } else {
+      this.stationary.x = false; 
     }
-    if (nextY < 0 || nextY >= BOARD_SIZE) {
-      this.yVelocity *= -1;
-    } else if (boardSquares[this.x][nextY].color === this.color) {
-      this.yVelocity *= -1;
-      boardSquares[this.x][nextY].color = this.color === lightColor ? darkColor : lightColor;
+
+    this.stationary.y = true;
+  
+    if (!isNextYInBounds) {
+      this.bounceVertically();
+    } else if (boardSquares[this.x][nextY].color === oppositeColor) {
+      this.bounceVertically();
+      boardSquares[this.x][nextY].color = this.color;
+    } else {
+      this.stationary.y = false;
     }
-    if(nextX > 0 && nextX < BOARD_SIZE && nextY > 0 && nextY < BOARD_SIZE && boardSquares[nextX][nextY].color === this.color) {
-      boardSquares[nextX][nextY].color = this.color;
-    }
-    this.x += this.xVelocity;
-    this.y += this.yVelocity;
+  
+    this.x += this.xVelocity * !this.stationary.x;
+    this.y += this.yVelocity * !this.stationary.y;
   };
+  
+
   render = () => {
     noStroke();
     fill(this.color);
-    ellipse((this.x + 0.5) * xUnit, (this.y + 0.5) * yUnit, ballDiameter, ballDiameter);
+    ellipse(
+      (this.x + 0.5) * xUnit,
+      (this.y + 0.5) * yUnit,
+      ballDiameter,
+      ballDiameter
+    );
   };
-};
-
+}
 function renderBoardSquares() {
   for (let i = 0; i < BOARD_SIZE; i++) {
     for (let j = 0; j < BOARD_SIZE; j++) {
       noStroke();
       fill(boardSquares[i][j].color);
       rect(i * xUnit, j * yUnit, xUnit, yUnit);
-    };
-  };
-};
-
+    }
+  }
+}
 function setup() {
   createCanvas(windowWidth, windowHeight);
   xUnit = windowWidth / BOARD_SIZE;
@@ -75,24 +104,22 @@ function setup() {
   darkColor = color(50);
   ballDiameter = min(xUnit, yUnit);
   initBoardSquares();
-  lightBouncingBall = new BouncingBall(lightColor,1,1);
-  darkBouncingBall = new BouncingBall(darkColor,-1,-1);
-};
-
+  lightBouncingBall = new BouncingBall(lightColor, 1, 1);
+  darkBouncingBall = new BouncingBall(darkColor, -1, -1);
+}
 function draw() {
   renderBoardSquares();
   darkBouncingBall.move();
   darkBouncingBall.render();
   lightBouncingBall.move();
   lightBouncingBall.render();
-};
-
+}
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   xUnit = windowWidth / BOARD_SIZE;
   yUnit = windowHeight / BOARD_SIZE;
   ballDiameter = min(xUnit, yUnit);
   initBoardSquares();
-  lightBouncingBall = new BouncingBall(lightColor,1,1);
-  darkBouncingBall = new BouncingBall(darkColor,-1,-1);
-};
+  lightBouncingBall = new BouncingBall(lightColor, 1, 1);
+  darkBouncingBall = new BouncingBall(darkColor, -1, -1);
+}
